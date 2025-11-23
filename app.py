ab4e33e45d266dd8c9c20abb1b94020bbac2a3d4
ibrahimelgharbi <33959915+ibrahimelgharbi@users.noreply.github.com>
2025-11-23 01:50:03 +0000

    Theme par defaut et logo

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ except ImportError:
 # ---------------------------------------------------------
 st.set_page_config(
     page_title="France Psoriasis – Analyse",
-    layout="wide"
+    page_icon="logo_france_psoriasis.png",  # le fichier doit être dans le même dossier que app.py
+    layout="wide",
+    theme={"base": "light"}
 )
 
 # Thème clair + marge haute pour que le header ne soit pas coupé
